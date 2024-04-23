7a13720040025324bfc48ad600d1ae1a78366046
fizbanstreasuryofdragons <167873534+fizbanstreasuryofdragons@users.noreply.github.com>
2024-04-23 13:44:10 +0200

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,18 @@
+basic.forever(function () {
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.pause(5000)
+    basic.pause(5000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
